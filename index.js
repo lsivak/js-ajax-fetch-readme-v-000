6946f62defaab495b45dc69997f1f953096eed5d
Lara Sanders <larasanders@gmail.com>
2018-07-22 17:55:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const app = "I don't do much.";
+
+const token = ' da4f675e128aa66a149793db2bead57d6c6490a6 '
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
