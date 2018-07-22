482843a9fb0df7cda534dee07b4ead7c99398712
Lara Sanders <larasanders@gmail.com>
2018-07-22 18:06:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = "I don't do much.";
 
 const token = ' da4f675e128aa66a149793db2bead57d6c6490a6 '
-fetch('https://api.github.com/user/repos', {
+fetch('https://api.github.com/lsivak/repos', {
   headers: {
     Authorization: `token ${token}`
   }
